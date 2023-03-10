3e817874c513b9ad5e05587ddb1a4cffb4ae0d06
chengying <hollye2558@gmail.com>
2023-03-10 14:46:26 -0500

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+wrap_parameters format:[]
+  def index
+    plants=Plant.all 
+    render json: plants, status: :ok
+  end
+
+  def show
+    plant = Plant.find_by(id:params[:id])
+    if plant
+      render json: plant, status: :ok
+    else
+      render json: {error:"plant not found"}, status: :not_found
+    end
+  end
+
+  def create
+    plant = Plant.create(params)
+    render json: plant, status: :created
+  end
+
+  private
+
+  def params
+    params.permit(:name, :image, :price)
+  end
 
 end
